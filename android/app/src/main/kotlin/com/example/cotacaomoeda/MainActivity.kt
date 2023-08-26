93922c6f8b7ad68def5f51d636cbb19cac02f0a8
Lucas Dickmann Schneider <Lucas.Schneider@proway.treina>
2023-08-25 21:59:26 -0300

    Primeiro commit do projeto

---
+++ android/app/src/main/kotlin/com/example/cotacaomoeda/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cotacaomoeda
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/convercao.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Convercao extends StatelessWidget {
+  const Convercao({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextFormField(
+
+          ),
+        ],
+      ),
+    );
+  }
+}
